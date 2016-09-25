1fab6641f7bc5f77078d487f85f861ec497b1c05
Leonid Azarenkov <leonid@rezonics.com>
2016-09-24 23:00:28 -0700

    minor fix

--- source/src/thumbsContainer.js
+++ source/src/thumbsContainer.js
@@ -384,7 +384,7 @@
       var zoomIcon = $('<div class="ThumbsViewer-zoomIco" />').append(aa);
 
       thumb_parent.append(zoomIcon);
-      thumb_parent.attr("title", "Открыть оригинал фото");
+      thumb_parent.attr("title", "Выбрать");
       thumb_parent.data(PluginName, {
         vk_img: vk_img
       });
